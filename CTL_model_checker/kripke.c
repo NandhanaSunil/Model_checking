0fee3e10d84eb62cf8ef30141b4da07fe6fdcb0b
NandhanaSunil <112201008@smail.iitpkd.ac.in>
2025-04-01 03:17:34 +0530

    processing kripke structures

---
+++ CTL_model_checker/kripke.c
@@ -0,0 +1,170 @@
+#include "kripke.h"
+
+void parseKripke(const char *filename, Kripke* ks){
+    // printf("Parsing Kripke structure from %s\n", filename);
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", filename);
+        exit(1);
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long fsize = ftell(fp);
+    rewind(fp);
+    printf("here\n");
+
+    char *json = malloc(fsize + 1);
+    fread(json, 1, fsize, fp);
+    fclose(fp);
+
+    json[fsize] = '\0';
+    cJSON *root = cJSON_Parse(json);
+    if (root == NULL) {
+        fprintf(stderr, "Error: Could not parse JSON file %s\n", filename);
+        exit(1);
+    }
+
+    cJSON *states = cJSON_GetObjectItem(root, "states");
+    cJSON *transitions = cJSON_GetObjectItem(root, "transitions");
+    cJSON *labels = cJSON_GetObjectItem(root, "labels");
+    // parse to get states
+    int i = 0;
+    cJSON *state;
+    cJSON_ArrayForEach(state, states) {
+        // printf("%s\n", state->valuestring);
+        strcpy(ks->states[i].name, state->valuestring);
+        i++;
+    }
+    ks->num_states = i;
+
+    // parse to get transitions
+    i = 0;
+    cJSON* from;
+    cJSON* to;
+    cJSON_ArrayForEach(from, transitions) {
+        cJSON_ArrayForEach(to, from) {
+            // printf("%s -> %s\n", from->string, to->valuestring);
+            strcpy(ks->transitions[i].from, from->string);
+            strcpy(ks->transitions[i].to, to->valuestring);
+            i++;
+        }
+    }
+    ks->num_transitions = i;
+
+    // parse to get labels
+    i = 0;
+    cJSON_ArrayForEach(from, labels) {
+        for (int j = 0; j < ks->num_states; j++) {
+            if (strcmp(ks->states[j].name, from->string) == 0) {
+                i = 0;
+                cJSON_ArrayForEach(to, from) {
+                    printf("%s\n", to->valuestring);
+                    strcpy(ks->states[j].labels[i], to->valuestring);
+                    i++;
+                }
+                ks->states[j].num_labels = i;
+                break;
+            }
+        }
+    }
+    free(json);
+    cJSON_Delete(root);
+}
+
+void printKripke(Kripke* ks){
+    printf("States:\n");
+    for (int i = 0; i < ks->num_states; i++) {
+        printf("  %s: ", ks->states[i].name);
+        for (int j = 0; j < ks->states[i].num_labels; j++) {
+            printf("%s ", ks->states[i].labels[j]);
+        }
+        printf("\n");
+    }
+    printf("Transitions:\n");
+    for (int i = 0; i < ks->num_transitions; i++) {
+        printf("  %s -> %s\n", ks->transitions[i].from, ks->transitions[i].to);
+    }
+}
+
+void freeKripke(Kripke* ks) {
+    for (int i = 0; i < ks->num_states; i++) {
+        if (ks->states[i].name) {
+            free(ks->states[i].name);
+            ks->states[i].name = NULL;
+        }
+
+        if (ks->states[i].labels) {
+            for (int j = 0; j < ks->states[i].num_labels; j++) {
+                if (ks->states[i].labels[j]) {
+                    free(ks->states[i].labels[j]);
+                }
+            }
+            free(ks->states[i].labels);  // Free the array of label pointers
+        }
+    }
+
+    for (int i = 0; i < ks->num_transitions; i++) {
+        if (ks->transitions[i].from) {
+            free(ks->transitions[i].from);
+        }
+        if (ks->transitions[i].to) {
+            free(ks->transitions[i].to);
+        }
+        free(ks->transitions);
+    }
+}
+
+
+
+Kripke initkripke(Kripke* ks){
+    //allocate memory for the kripke structure
+    ks = (Kripke*)malloc(sizeof(Kripke));
+    if (ks == NULL) {
+        fprintf(stderr, "Error: Could not allocate memory for Kripke structure\n");
+        exit(1);
+    }
+    //allocate memory for the states
+    for (int i = 0; i < MAX_STATES; i++) {
+        ks->states[i].name = (char*)malloc(50);
+        if (ks->states[i].name == NULL) {
+            fprintf(stderr, "Error: Could not allocate memory for state name\n");
+            exit(1);
+        }
+        ks->states[i].labels = (char**)malloc(MAX_LABELS * sizeof(char*));
+        if (ks->states[i].labels == NULL) {
+            perror("Memory allocation failed for state labels");
+            exit(EXIT_FAILURE);
+        }
+        for (int j = 0; j < MAX_LABELS; j++) {
+            ks->states[i].labels[j] = (char*)malloc(50);
+            if (ks->states[i].labels[j] == NULL) {
+                fprintf(stderr, "Error: Could not allocate memory for state label\n");
+                exit(1);
+            }
+        }
+        ks->states[i].num_labels = 0;
+        ks->states[i].visited = 0;
+        for (int i = 0; i < MAX_STATES * 3; i++) {
+            ks->transitions[i].from = (char*)malloc(50 * sizeof(char));
+            ks->transitions[i].to = (char*)malloc(50 * sizeof(char));
+            if (ks->transitions[i].from == NULL || ks->transitions[i].to == NULL) {
+                perror("Memory allocation failed for transitions");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    return *ks;
+}
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <kripke.json>\n", argv[0]);
+        return 1;
+    }
+
+    Kripke ks;
+    ks = initkripke(&ks);
+    parseKripke(argv[1], &ks);
+    printKripke(&ks);
+    freeKripke(&ks);
+    return 0;
+}
---
+++ CTL_model_checker/kripke.h
@@ -0,0 +1,33 @@
+#ifndef KRIPKE_H
+#define KRIPKE_H
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include "cJSON.h"
+
+#define MAX_STATES 10
+#define MAX_LABELS 10
+typedef struct {
+    char* name;         // State name
+    char** labels;   // Atomic propositions
+    int num_labels;     // Number of atomic propositions
+    int visited;        // Used for DFS
+} State;
+
+typedef struct {
+    char* from;  // Start state
+    char* to;    // End state
+} Transition;
+
+typedef struct {
+    State states[MAX_STATES];
+    Transition transitions[MAX_STATES * 2];  // Adjacency list representation
+    int num_states;
+    int num_transitions;
+} Kripke;
+
+void parseKripke(const char *filename, Kripke* ks);
+void printKripke(Kripke* ks);
+void freeKripke(Kripke* ks);
+#endif
